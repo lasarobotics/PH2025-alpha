6766b6ed9293a297556bb8ab5faa6448fce6e973
Liam Teltow <liam.teltow@gmail.com>
2025-02-03 19:12:41 -0600

    Initial setup from PurpleSwerve

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,137 @@
+package main.java.frc.robot;
+
+import java.util.Optional;
+
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
+import org.lasarobotics.drive.swerve.AdvancedSwerveKinematics.ControlCentricity;
+import org.lasarobotics.drive.swerve.DriveWheel;
+import org.lasarobotics.drive.swerve.child.MAXSwerveModule;
+import org.lasarobotics.drive.swerve.child.SwerveX2Module;
+import org.lasarobotics.hardware.kauailabs.NavX2;
+import org.lasarobotics.hardware.revrobotics.Spark;
+import org.lasarobotics.utils.FFConstants;
+import org.lasarobotics.utils.PIDConstants;
+import org.lasarobotics.vision.AprilTagCamera.Resolution;
+
+import com.pathplanner.lib.config.ModuleConfig;
+
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Dimensionless;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Mass;
+import edu.wpi.first.units.measure.Time;
+
+public final class Constants {
+
+  public static class Field {
+    public static final AprilTagFieldLayout FIELD_LAYOUT = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
+    public static final AprilTag BLUE_SPEAKER = getTag(7).get();
+    public static final AprilTag RED_SPEAKER = getTag(4).get();
+    public static final AprilTag BLUE_AMP = getTag(6).get();
+
+
+    /**
+     * Get AprilTag from field
+     * @param id Tag ID
+     * @return AprilTag matching ID
+     */
+    public static Optional<AprilTag> getTag(int id) {
+      return FIELD_LAYOUT.getTags().stream().filter((tag) -> tag.ID == id).findFirst();
+    }
+  }
+
+  public static class HID {
+    public static final int PRIMARY_CONTROLLER_PORT = 0;
+    public static final int SECONDARY_CONTROLLER_PORT = 1;
+    public static final Dimensionless CONTROLLER_DEADBAND = Units.Percent.of(10);
+  }
+
+  public static class SmartDashboard {
+    public static final String SMARTDASHBOARD_DEFAULT_TAB = "SmartDashboard";
+    public static final String SMARTDASHBOARD_AUTO_MODE = "Auto Mode";
+  }
+
+  public static class Drive {
+    public static final DriveWheel DRIVE_WHEEL = DriveWheel.create(Units.Inches.of(4.0), Units.Value.of(1.3), Units.Value.of(1.2));
+    public static final PIDConstants DRIVE_PID = PIDConstants.of(0.3, 0.0, 0.001, 0.0, 0.0);
+    public static final FFConstants DRIVE_FF = FFConstants.of(0.2, 0.0, 0.0, 0.0);
+    public static final PIDConstants ROTATE_PID = PIDConstants.of(2.0, 0.0, 0.1, 0.0, 0.0);
+    public static final FFConstants ROTATE_FF = FFConstants.of(0.2, 0.0, 0.0, 0.0);
+    public static final PIDConstants DRIVE_ROTATE_PID = PIDConstants.of(8.0, 0.0, 0.3, 0.0, 0.0);
+    public static final PIDConstants DRIVE_AUTO_AIM_PID = PIDConstants.of(12.0, 0.0, 0.1, 0.0, 0.0);
+    public static final Dimensionless DRIVE_SLIP_RATIO = Units.Percent.of(3.0);
+    public static final Angle DRIVE_TURN_SCALAR = Units.Degrees.of(90.0);
+    public static final Time DRIVE_LOOKAHEAD = Units.Seconds.of(0.2);
+
+    public static final Distance DRIVE_WHEELBASE = Units.Meters.of(0.5588);
+    public static final Distance DRIVE_TRACK_WIDTH = Units.Meters.of(0.5588);
+    public static final Mass MASS = Units.Pounds.of(110.0);
+    public static final Time AUTO_LOCK_TIME = Units.Seconds.of(3.0);
+    public static final Current DRIVE_CURRENT_LIMIT = Units.Amps.of(90.0);
+
+    public static final ControlCentricity DRIVE_CONTROL_CENTRICITY = ControlCentricity.FIELD_CENTRIC;
+
+    private static final double DRIVE_THROTTLE_INPUT_CURVE_X[] = { 0.0, 0.100, 0.200, 0.300, 0.400, 0.500, 0.600, 0.700, 0.800, 0.900, 1.000 };
+    private static final double DRIVE_THROTTLE_INPUT_CURVE_Y[] = { 0.0, 0.052, 0.207, 0.465, 0.827, 1.293, 1.862, 2.534, 3.310, 4.189, 5.172 };
+    private static final double DRIVE_TURN_INPUT_CURVE_X[] = { 0.0, 0.100, 0.200, 0.300, 0.400, 0.500, 0.600, 0.700, 0.800, 0.900, 1.0 };
+    private static final double DRIVE_TURN_INPUT_CURVE_Y[] = { 0.0, 0.010, 0.050, 0.100, 0.150, 0.200, 0.250, 0.300, 0.400, 0.600, 1.0 };
+
+    private static final SplineInterpolator SPLINE_INTERPOLATOR = new SplineInterpolator();
+    public static final PolynomialSplineFunction DRIVE_THROTTLE_INPUT_CURVE = SPLINE_INTERPOLATOR.interpolate(DRIVE_THROTTLE_INPUT_CURVE_X, DRIVE_THROTTLE_INPUT_CURVE_Y);
+    public static final PolynomialSplineFunction DRIVE_TURN_INPUT_CURVE = SPLINE_INTERPOLATOR.interpolate(DRIVE_TURN_INPUT_CURVE_X, DRIVE_TURN_INPUT_CURVE_Y);
+
+    public static final SwerveX2Module.GearRatio CTRE_GEAR_RATIO = SwerveX2Module.GearRatio.X4_3;
+    public static final MAXSwerveModule.GearRatio REV_GEAR_RATIO = MAXSwerveModule.GearRatio.L3;
+
+    public static final ModuleConfig MODULE_CONFIG = new ModuleConfig(DRIVE_WHEEL.diameter.div(2), Units.MetersPerSecond.of(5.172), 1.3, DCMotor.getKrakenX60Foc(1), DRIVE_CURRENT_LIMIT, 1);
+  }
+
+  public static class REVDriveHardware {
+    public static final NavX2.ID NAVX_ID = new NavX2.ID("DriveHardware/NavX2");
+    public static final Spark.ID LEFT_FRONT_DRIVE_MOTOR_ID = new Spark.ID("DriveHardware/Swerve/LeftFront/Drive", 2);
+    public static final Spark.ID LEFT_FRONT_ROTATE_MOTOR_ID = new Spark.ID("DriveHardware/Swerve/LeftFront/Rotate", 3);
+    public static final Spark.ID RIGHT_FRONT_DRIVE_MOTOR_ID = new Spark.ID("DriveHardware/Swerve/RightFront/Drive", 4);
+    public static final Spark.ID RIGHT_FRONT_ROTATE_MOTOR_ID = new Spark.ID("DriveHardware/Swerve/RightFront/Rotate", 5);
+    public static final Spark.ID LEFT_REAR_DRIVE_MOTOR_ID = new Spark.ID("DriveHardware/Swerve/LeftRear/Drive", 6);
+    public static final Spark.ID LEFT_REAR_ROTATE_MOTOR_ID = new Spark.ID("DriveHardware/Swerve/LeftRear/Rotate", 7);
+    public static final Spark.ID RIGHT_REAR_DRIVE_MOTOR_ID = new Spark.ID("DriveHardware/Swerve/RightRear/Drive", 8);
+    public static final Spark.ID RIGHT_REAR_ROTATE_MOTOR_ID = new Spark.ID("DriveHardware/Swerve/RightRear/Rotate", 9);
+  }
+
+  public static class VisionHardware {
+    public static final String CAMERA_A_NAME = "Arducam_OV9782_USB_Camera_A";
+    public static final Transform3d CAMERA_A_LOCATION = new Transform3d(
+      new Translation3d(-0.1016, -0.2921, 0.521),
+      new Rotation3d(0.0, Math.toRadians(-26.0), Math.toRadians(+180.0))
+    );
+    public static final Resolution CAMERA_A_RESOLUTION = Resolution.RES_1280_720;
+    public static final Rotation2d CAMERA_A_FOV = Rotation2d.fromDegrees(79.7);
+
+    public static final String CAMERA_B_NAME = "Arducam_OV9782_USB_Camera_B";
+    public static final Transform3d CAMERA_B_LOCATION = new Transform3d(
+      new Translation3d(0.0254, -0.2921, 0.584),
+      new Rotation3d(0.0, Math.toRadians(-25.6), 0.0)
+    );
+    public static final Resolution CAMERA_B_RESOLUTION = Resolution.RES_1280_720;
+    public static final Rotation2d CAMERA_B_FOV = Rotation2d.fromDegrees(79.7);
+
+    public static final String CAMERA_OBJECT_NAME = "Arducam_OV9782_USB_Camera_C";
+    public static final Transform3d CAMERA_OBJECT_LOCATION = new Transform3d(
+      new Translation3d(0.3, 0.0, 0.5),
+      new Rotation3d(0, Math.toRadians(+15.0), Math.toRadians(180))
+    );
+    public static final Resolution CAMERA_OBJECT_RESOLUTION = Resolution.RES_1280_720;
+    public static final Rotation2d CAMERA_OBJECT_FOV = Rotation2d.fromDegrees(79.7);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,207 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.drive;
+
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
+import org.lasarobotics.drive.swerve.AdvancedSwerveKinematics.ControlCentricity;
+import org.lasarobotics.drive.swerve.SwerveDrive;
+import org.lasarobotics.drive.swerve.SwerveModule;
+import org.lasarobotics.drive.swerve.child.MAXSwerveModule;
+import org.lasarobotics.drive.swerve.parent.REVSwerveModule;
+import org.lasarobotics.hardware.kauailabs.NavX2;
+import org.lasarobotics.hardware.revrobotics.Spark;
+import org.lasarobotics.utils.PIDConstants;
+import org.lasarobotics.vision.AprilTagCamera;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Dimensionless;
+import edu.wpi.first.units.measure.Time;
+import edu.wpi.first.wpilibj.DriverStation;
+import main.java.frc.robot.Constants;
+
+public class DriveSubsystem extends SwerveDrive {
+
+  Hardware m_drivetrainHardware;
+
+  /**
+   * Create an instance of DriveSubsystem
+   * <p>
+   * NOTE: ONLY ONE INSTANCE SHOULD EXIST AT ANY TIME!
+   * <p>
+   * @param drivetrainHardware Hardware devices required by drivetrain
+   * @param rotatePIDF PID gains for rotate PID
+   * @param aimPIDF PID gains for aim PID
+   * @param controlCentricity Control centricity
+   * @param throttleInputCurve Spline function characterising throttle input
+   * @param rotateInputCurve Spline function characterising rotate input
+   * @param rotateScalar Scalar for rotate input
+   * @param deadband Deadband for controller input [+0.001, +0.2]
+   * @param lookAhead Rotate PID lookahead
+   */
+  public DriveSubsystem(Hardware drivetrainHardware, PIDConstants rotatePIDF, PIDConstants aimPIDF, ControlCentricity controlCentricity,
+                        PolynomialSplineFunction throttleInputCurve, PolynomialSplineFunction rotateInputCurve,
+                        Angle rotateScalar, Dimensionless deadband, Time lookAhead) {
+    super(drivetrainHardware, rotatePIDF, aimPIDF, controlCentricity, throttleInputCurve, rotateInputCurve, rotateScalar, deadband, lookAhead);
+    m_drivetrainHardware = drivetrainHardware;
+  }
+
+  /**
+   * Initialize hardware devices for drive subsystem
+   * @return Hardware object containing all necessary devices for this subsystem
+   */
+  public static Hardware initializeHardware() {
+    NavX2 navx = new NavX2(Constants.REVDriveHardware.NAVX_ID);
+
+    REVSwerveModule lFrontModule = MAXSwerveModule.create(
+      REVSwerveModule.initializeHardware(
+        Constants.REVDriveHardware.LEFT_FRONT_DRIVE_MOTOR_ID,
+        Constants.REVDriveHardware.LEFT_FRONT_ROTATE_MOTOR_ID,
+        Spark.MotorKind.NEO_VORTEX,
+        Spark.MotorKind.NEO_550
+      ),
+      SwerveModule.Location.LeftFront,
+      Constants.Drive.REV_GEAR_RATIO,
+      Constants.Drive.DRIVE_WHEEL,
+      Constants.Drive.DRIVE_PID,
+      Constants.Drive.DRIVE_FF,
+      Constants.Drive.ROTATE_PID,
+      Constants.Drive.ROTATE_FF,
+      Constants.Drive.DRIVE_SLIP_RATIO,
+      Constants.Drive.MASS,
+      Constants.Drive.DRIVE_WHEELBASE,
+      Constants.Drive.DRIVE_TRACK_WIDTH,
+      Constants.Drive.AUTO_LOCK_TIME,
+      Constants.Drive.DRIVE_CURRENT_LIMIT
+    );
+
+    REVSwerveModule rFrontModule = MAXSwerveModule.create(
+      REVSwerveModule.initializeHardware(
+        Constants.REVDriveHardware.RIGHT_FRONT_DRIVE_MOTOR_ID,
+        Constants.REVDriveHardware.RIGHT_FRONT_ROTATE_MOTOR_ID,
+        Spark.MotorKind.NEO_VORTEX,
+        Spark.MotorKind.NEO_550
+      ),
+      SwerveModule.Location.RightFront,
+      Constants.Drive.REV_GEAR_RATIO,
+      Constants.Drive.DRIVE_WHEEL,
+      Constants.Drive.DRIVE_PID,
+      Constants.Drive.DRIVE_FF,
+      Constants.Drive.ROTATE_PID,
+      Constants.Drive.ROTATE_FF,
+      Constants.Drive.DRIVE_SLIP_RATIO,
+      Constants.Drive.MASS,
+      Constants.Drive.DRIVE_WHEELBASE,
+      Constants.Drive.DRIVE_TRACK_WIDTH,
+      Constants.Drive.AUTO_LOCK_TIME,
+      Constants.Drive.DRIVE_CURRENT_LIMIT
+    );
+
+    REVSwerveModule lRearModule = MAXSwerveModule.create(
+      REVSwerveModule.initializeHardware(
+        Constants.REVDriveHardware.LEFT_REAR_DRIVE_MOTOR_ID,
+        Constants.REVDriveHardware.LEFT_REAR_ROTATE_MOTOR_ID,
+        Spark.MotorKind.NEO_VORTEX,
+        Spark.MotorKind.NEO_550
+      ),
+      SwerveModule.Location.LeftRear,
+      Constants.Drive.REV_GEAR_RATIO,
+      Constants.Drive.DRIVE_WHEEL,
+      Constants.Drive.DRIVE_PID,
+      Constants.Drive.DRIVE_FF,
+      Constants.Drive.ROTATE_PID,
+      Constants.Drive.ROTATE_FF,
+      Constants.Drive.DRIVE_SLIP_RATIO,
+      Constants.Drive.MASS,
+      Constants.Drive.DRIVE_WHEELBASE,
+      Constants.Drive.DRIVE_TRACK_WIDTH,
+      Constants.Drive.AUTO_LOCK_TIME,
+      Constants.Drive.DRIVE_CURRENT_LIMIT
+    );
+
+    REVSwerveModule rRearModule = MAXSwerveModule.create(
+      REVSwerveModule.initializeHardware(
+        Constants.REVDriveHardware.RIGHT_REAR_DRIVE_MOTOR_ID,
+        Constants.REVDriveHardware.RIGHT_REAR_ROTATE_MOTOR_ID,
+        Spark.MotorKind.NEO_VORTEX,
+        Spark.MotorKind.NEO_550
+      ),
+      SwerveModule.Location.RightRear,
+      Constants.Drive.REV_GEAR_RATIO,
+      Constants.Drive.DRIVE_WHEEL,
+      Constants.Drive.DRIVE_PID,
+      Constants.Drive.DRIVE_FF,
+      Constants.Drive.ROTATE_PID,
+      Constants.Drive.ROTATE_FF,
+      Constants.Drive.DRIVE_SLIP_RATIO,
+      Constants.Drive.MASS,
+      Constants.Drive.DRIVE_WHEELBASE,
+      Constants.Drive.DRIVE_TRACK_WIDTH,
+      Constants.Drive.AUTO_LOCK_TIME,
+      Constants.Drive.DRIVE_CURRENT_LIMIT
+    );
+
+    AprilTagCamera frontCamera = new AprilTagCamera(
+      Constants.VisionHardware.CAMERA_A_NAME,
+      Constants.VisionHardware.CAMERA_A_LOCATION,
+      Constants.VisionHardware.CAMERA_A_RESOLUTION,
+      Constants.VisionHardware.CAMERA_A_FOV,
+      Constants.Field.FIELD_LAYOUT
+    );
+
+    AprilTagCamera rearCamera = new AprilTagCamera(
+      Constants.VisionHardware.CAMERA_B_NAME,
+      Constants.VisionHardware.CAMERA_B_LOCATION,
+      Constants.VisionHardware.CAMERA_B_RESOLUTION,
+      Constants.VisionHardware.CAMERA_B_FOV,
+      Constants.Field.FIELD_LAYOUT
+    );
+
+    Hardware drivetrainHardware = new Hardware(navx, lFrontModule, rFrontModule, lRearModule, rRearModule, frontCamera, rearCamera);
+
+    return drivetrainHardware;
+  }
+
+  /**
+   * Configures the AutoBuilder so that PathPlanner can use its built-in commands when running autos.
+   * Uses the PathPlanner AutoBuilder method.
+   */
+  public void configureAutoBuilder() {
+    AutoBuilder.configure(
+      () -> getPose(), // Robot pose supplier
+      (pose) -> resetPose(pose), // Method to reset odometry (will be called if your auto has a starting pose)
+      () -> getChassisSpeeds(), // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+      (speeds, feedforwards) -> autoDrive(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+      new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+        new com.pathplanner.lib.config.PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+        new com.pathplanner.lib.config.PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+      ),
+      new RobotConfig(
+        Constants.Drive.MASS, // The robot configuration
+        Units.KilogramSquareMeters.of(1),
+        Constants.Drive.MODULE_CONFIG,
+        m_drivetrainHardware.lFrontModule().getModuleCoordinate(),
+        m_drivetrainHardware.rFrontModule().getModuleCoordinate(),
+        m_drivetrainHardware.lRearModule().getModuleCoordinate(),
+        m_drivetrainHardware.rRearModule().getModuleCoordinate()
+      ),
+      () -> {
+        // Boolean supplier that controls when the path will be mirrored for the red alliance
+        // This will flip the path being followed to the red side of the field.
+        // THE ORIGIN WILL REMAIN ON THE BLUE SI
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+          return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+      },
+      this // Reference to this subsystem to set requirements
+    );
+  }
+}
